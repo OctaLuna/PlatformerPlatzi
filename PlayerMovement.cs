526db6aff8758d3a2d3af0280990944b77e14586
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-02 11:39:16 -0400

    Commit 1, en esta parte se codifico el el movimiento y salto del Player, el script se llama PlayerMovement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //VARIABLES//
+    //estamos haciendo referencia al RigidBody2D del Player
+    public Rigidbody2D RBPlayer;
+    //La velocidad del Player en el movimiento en eje X
+    public float PlayerSpeed = 2;
+    //La velocidad del salto del Player
+    public float JumpSpeed = 30;
+    //Esto es para poder hacer 
+    private bool JumpGround = false;
+
+
+
     void Start()
     {
         
@@ -13,6 +24,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Esto es para el movimiento del personaje
+        //RBPlayer.velocity nos estamos refiriendo a la variable que creamos posteriormente
+        //.velocity esta es la velocidad del RigidBody2D
+        //new Vector2() estamos usando un nuevo vector, que es el Vector 2, que tiene solo 2 ejes que son "x" y "y"
+        //Input.GetAxis("Horizontal") * Speed, Input.GetAxis esta es una funcion de Unity que son variables para poder controlar el player con el teclado
+        //RBPlayer.velocity.y  estamos haciendo referencia a la variable de RBPlayer, estamos accediendo a su funcion .velocity en eje y 
+        RBPlayer.velocity = new Vector2(Input.GetAxis("Horizontal") * PlayerSpeed, RBPlayer.velocity.y);
         
+        //
+        if(Input.GetKeyDown(KeyCode.Space)){
+            //
+            RBPlayer.AddForce(Vector2.up * JumpSpeed);
+        }
+    
     }
 }
