a9891b7ac12733ac3cd334616efaf35d850fd013
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-03 10:20:51 -0400

    Commit 2, se modifico el nombre del script de el player era PlayerMovement y ahora es PlayerMovementGame

--- PlayerMovementGame.cs
+++ PlayerMovementGame.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementGame : MonoBehaviour
 {
     //VARIABLES//
     //estamos haciendo referencia al RigidBody2D del Player
