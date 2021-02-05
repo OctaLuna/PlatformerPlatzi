dcc27a22a942533e55e9ef0acb021b88d061ea1d
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-04 20:11:01 -0400

    Commit 3, en este commit se agrego las animacion del player de Jump, Walking, es el script PlayerMovementGame

--- PlayerMovementGame.cs
+++ PlayerMovementGame.cs
@@ -11,15 +11,11 @@ public class PlayerMovementGame : MonoBehaviour
     public float PlayerSpeed = 2;
     //La velocidad del salto del Player
     public float JumpSpeed = 30;
-    //Esto es para poder hacer 
-    private bool JumpGround = false;
+    //Esto es una variable para ver si estamos tocando el piso 
+    bool isGrounded = true;
+    //
+    public Animator PlayerAnimator;
 
-
-
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -32,11 +28,47 @@ public class PlayerMovementGame : MonoBehaviour
         //RBPlayer.velocity.y  estamos haciendo referencia a la variable de RBPlayer, estamos accediendo a su funcion .velocity en eje y 
         RBPlayer.velocity = new Vector2(Input.GetAxis("Horizontal") * PlayerSpeed, RBPlayer.velocity.y);
         
-        //
-        if(Input.GetKeyDown(KeyCode.Space)){
-            //
-            RBPlayer.AddForce(Vector2.up * JumpSpeed);
+        //Esto es para las animacion del Player
+        //Esto Input.GetAxis("Horizontal") == 0) esto llama a la variable Horizontal que son las del teclado, -1 es inzquierda, 0 es quieto y 1 es derecha
+        if(Input.GetAxis("Horizontal") == 0){
+            //Estamos accediento a la variable isWalking que es la del movimiento del player
+            //PlayerAnimator es la variable que hace referencia a las animaciones del Player
+            //.SetBool() esto te pregunta que variable es a la cual quieres llamar en nuestro caso es boleana la variable
+            //("isWalking", false) Esta es la variable del movimiento si esta en falso es que esta quieto
+            PlayerAnimator.SetBool("isWalking", false);
         }
+        else if(Input.GetAxis("Horizontal") < 0){
+            PlayerAnimator.SetBool("isWalking", true);
+            //Estamots llamando a un funcion que tenemos en el player que es Spriterenderer y estamos llamndo a la funcion flipX con la cual podemos rotar el sprite del player
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if(Input.GetAxis("Horizontal") > 0){
+            PlayerAnimator.SetBool("isWalking", true);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+
+        //Este if es para ver si estamos tocando el piso, para no saltar 2 veces
+        if (isGrounded){
+            //Input.GetKeyDown(KeyCode.Space) es una variable de unity que hace referencia las teclas del teclado
+            //En este caso estamos usando la tecla de Space
+            if(Input.GetKeyDown(KeyCode.Space)){
+                //Esto es para que el player pueda saltar 
+                RBPlayer.AddForce(Vector2.up * JumpSpeed);
+                //Este es para que cuando saltemos en la variable boleana que creamos previamente se ponga en false por que no estamos tocando el suelo
+                isGrounded = false;
+                //
+                PlayerAnimator.SetTrigger("Jump");
+            }    
+        }
+        
     
     }
+    //Esto es para saber si estamos tocando el piso 
+    private void OnCollisionEnter2D(Collision2D Collision) {
+        if(Collision.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
+    }
 }
+
