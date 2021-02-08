078afccda15b7b2da50bead467675ec701d05f12
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-08 13:26:17 -0400

    Commit 6, se implemento las animaciones del enemigo y la condicion de muerte del enemy, se modifico el script EnemyBehaviour

--- EnemyBehaviour.cs
+++ EnemyBehaviour.cs
@@ -4,25 +4,66 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    //Estamos haciendo referencia a el rigidBody2D del enemy
     Rigidbody2D enemyRB;
+    //Esto es el tiempo antes del cambio de movimiento de derecha a izquierda
     float timeBeforeChange;
+    //Este es el tiempo en el cual va a ir a un lado
     public float delay = .5f;
+    //Esta es la velocidad del enemy
     public float Speed = .3f;
+    //Estamos haciendo referencia a la animacion del enemy
+    Animator EnemyAnim;
     
     
     // Start is called before the first frame update
     void Start()
     {
+        //enemy RB es para llamar a el Rigidbody2D del enemy
         enemyRB = GetComponent<Rigidbody2D>();
+        //enemyAnim es para llamar al animator del enemy
+        EnemyAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Esto es para que se mueva el enemy 
         enemyRB.velocity = Vector2.right * Speed;
+        
+        ////Esto es para las animaciones del enemy
+        if(Speed > 0){
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if(Speed < 0){
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        ////
+        //Esto es para el movimiento de derecha a izquierda del player
         if(timeBeforeChange < Time.time){   
             Speed *= -1; 
             timeBeforeChange = Time.time + delay;
+            
         }
     }
+
+    //Esto es para destruir al enemy, es cuando el player salta encima de el 
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //Esto pasara solo con los gameObject del Player
+        if(collision.gameObject.tag == "Player"){
+            //Esto es para que el player pueda pisarlo, estos solo susedera cuando el trasform.y es mayor al del enemy
+            if(transform.position.y + .03f < collision.transform.position.y){
+                //Esto es para poner la variable de animacion en true de la muerte del player
+                EnemyAnim.SetBool("isDead", true);
+                
+            }
+        }
+    }
+    
+    //esto es para matar al enemy, cuando acabe la animacion del enemigo sucedera esto
+    public void DisableEnemy(){
+        gameObject.SetActive(false);
+    }
+    
+
 }
