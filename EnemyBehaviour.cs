53959d338154cac27c7e91c5838bde64fa7939e0
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-08 11:40:20 -0400

    Commit 5, se programo el movimiento del enemigo, el script se llama EnemyBehaviour

---
+++ EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    Rigidbody2D enemyRB;
+    float timeBeforeChange;
+    public float delay = .5f;
+    public float Speed = .3f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRB.velocity = Vector2.right * Speed;
+        if(timeBeforeChange < Time.time){   
+            Speed *= -1; 
+            timeBeforeChange = Time.time + delay;
+        }
+    }
+}
