7461078c726916c97b29d15c792c0c36aef2d2d6
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-09 15:36:45 -0400

    Commit 7, se programo al las monedas del juego, el script Collectable

---
+++ Collectable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable : MonoBehaviour
+{
+    //VARIABLES//
+    public int CollectableQuantity = 0;
+    public Text CollectableText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.gameObject.tag == "Player"){
+            Destroy(gameObject);
+            CollectableQuantity++;
+            CollectableText.text = CollectableQuantity.ToString();
+        }
+        
+    }
+}
