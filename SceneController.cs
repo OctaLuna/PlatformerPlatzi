9dbcbd5933a39c3ab060792ecd29cccc1278a2c5
Marco Octavio Luna Vargas <marco.octavio.luna@gmail.com>
2021-02-10 17:48:51 -0400

    Commit 11, se programo la meta del nivel, y el cambio de escenas, se modificaron los script SceneController y WinController

---
+++ SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    //Esto es para cambiar a la scena que queramos con su nombre
+    public void ChangeSceneTo(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+    //Esto es para ir a la siguiente escena de la cronologia
+    public void NextScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
--- WinController.cs
+++ WinController.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WinController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public SceneController SceneController;
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.tag == "Player"){
+            SceneController.NextScene();
+        }
     }
 }
